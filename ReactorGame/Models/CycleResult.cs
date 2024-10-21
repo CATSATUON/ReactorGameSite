2fd9b6cd68780cd513c22b37bbf379062e35d2d3
MartynCox <94151909+MartynCox@users.noreply.github.com>
2024-10-21 22:30:00 +1100

    Tank capacity

--- ReactorGame/Models/CycleResult.cs
+++ ReactorGame/Models/CycleResult.cs
@@ -6,7 +6,10 @@ namespace ReactorGame.Models
     public class CycleResult
     {
         [JsonProperty("valvesFlows")]
-        public List<int> ValveFlows { get; set;}
+        public List<int> ValveFlows { get; set; }
+
+        [JsonProperty("tankStatus")]
+        public List<float> TankStatus { get; set; }
 
         [JsonProperty("temperature")]
         public float ReactorTemperature { get; set; }
@@ -23,15 +26,17 @@ namespace ReactorGame.Models
         public CycleResult()
         {
             ValveFlows = new List<int>();
+            TankStatus = new List<float>();
             ReactorTemperature = 0;
             Step = 0;
             InGameTime = 0;
             RealTime = 0;
         }
 
-        public CycleResult(List<int> valveFlows, float reactorTemperature, int step, float inGameTime, float realTime)
+        public CycleResult(List<int> valveFlows, List<float> tankStatus, float reactorTemperature, int step, float inGameTime, float realTime)
         {
             ValveFlows = valveFlows;
+            TankStatus = tankStatus;
             ReactorTemperature = reactorTemperature;
             Step = step;
             InGameTime = inGameTime;
@@ -41,7 +46,7 @@ namespace ReactorGame.Models
         public static string GetHeader()
         {
             return "Cycle, Temperature, In-game time, Real time, Valve1, Valve2, Valve3, Valve4, Valve5, Valve 6,"
-                + "Valve 7, Valve 8, Valve 9, Valve 10, Valve 11, Valve 12, Valve 13";
+                + "Valve 7, Valve 8, Valve 9, Valve 10, Valve 11, Valve 12, Valve 13, Tank 1, Tank 2, Tank 3, Tank 4";
         }
 
         public string ToCSV()
@@ -50,7 +55,8 @@ namespace ReactorGame.Models
                 + "," + ReactorTemperature.ToString()
                 + "," + InGameTime.ToString()
                 + "," + RealTime.ToString()
-                + "," + string.Join(",", ValveFlows);
+                + "," + string.Join(",", ValveFlows)
+                + "," + string.Join(",", TankStatus);
         }
     }
 }
